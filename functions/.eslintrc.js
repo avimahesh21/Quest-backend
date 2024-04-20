7c07ab490b57539f1d867e628f00c170934b6acb
avi22vt <avi22@vt.edu>
2024-04-20 16:22:11 -0400

    lots of functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -29,5 +29,8 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "object-curly-spacing": ["error", "never"],
+    "prefer-const": "error",
+    "max-len": ["error", {"code": 100}],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,211 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import {getFirestore, Timestamp} from "firebase-admin/firestore";
+import * as admin from "firebase-admin";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const getQuest = onRequest((request, response) => {
+  const db = getFirestore();
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set to start of the day
+
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1); // Next day
+
+  const todayStart = Timestamp.fromDate(today);
+  const todayEnd = Timestamp.fromDate(tomorrow);
+
+  db.collection("Quests")
+    .where("timestamp", ">=", todayStart)
+    .where("timestamp", "<", todayEnd)
+    .get()
+    .then((snapshot) => {
+      const quests: any[] = [];
+
+      if (snapshot.empty) {
+        response.status(404).send({message: "No quests found for today."});
+        return;
+      }
+
+      snapshot.forEach((doc) => {
+        const quest = {
+          id: doc.id,
+          description: doc.data().Desc,
+          timestamp: doc.data().timestamp.toDate(), // Assuming 'timestamp' is stored in Firestore
+        };
+        quests.push(quest);
+      });
+
+      response.status(200).json(quests); // Send quests as JSON
+    })
+    .catch((error) => {
+      console.error("Error fetching quests: ", error);
+      response.status(500).send({error: error.message});
+    });
+});
+
+
+export const getAllPosts = onRequest((request, response) => {
+  const db = getFirestore();
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set to start of today
+
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1); // Start of tomorrow
+
+  const todayStart = Timestamp.fromDate(today);
+  const todayEnd = Timestamp.fromDate(tomorrow);
+
+
+  db.collection("Posts")
+    .where("Date", ">=", todayStart)
+    .where("Date", "<", todayEnd)
+    .get()
+    .then((snapshot) => {
+      const posts: any[] = [];
+
+      if (snapshot.empty) {
+        response.status(404).send({message: "No quests found for today."});
+        return;
+      }
+
+      snapshot.forEach((doc) => {
+        const post = {
+          DatePosted: doc.data().Date,
+          ImageData: doc.data().ImageData,
+          LocationData: doc.data().LocationData,
+          Note: doc.data().Note,
+          PostID: doc.data().PostID,
+          Upvotes: doc.data().Upvotes,
+          UserID: doc.data().UserID,
+        };
+        posts.push(post);
+      });
+
+      response.status(200).json(posts); // Send quests as JSON
+    })
+    .catch((error) => {
+      console.error("Error fetching posts: ", error);
+      response.status(500).send({error: error.message});
+    });
+});
+
+
+export const getTopUsersByVotes = onRequest((request, response) => {
+  const db = getFirestore();
+  db.collection("Users")
+    .orderBy("Upvotes", "desc") // Order by 'upvotes' in descending order
+    .get()
+    .then((snapshot) => {
+      const users: any[] = [];
+
+      snapshot.forEach((doc) => {
+        const user = {
+          UserID: doc.data().UserID,
+          Upvotes: doc.data().Upvotes,
+          Streak: doc.data().Streak,
+        };
+        users.push(user);
+      });
+
+      response.status(200).json(users); // Send sorted users as JSON
+    })
+    .catch((error) => {
+      console.error("Error fetching users: ", error);
+      response.status(500).send({error: error.message});
+    });
+});
+
+
+export const getTopUsersByStreaks = onRequest((request, response) => {
+  const db = getFirestore();
+  db.collection("Users")
+    .orderBy("Streak", "desc") // Order by 'upvotes' in descending order
+    .get()
+    .then((snapshot) => {
+      const users: any[] = [];
+
+      snapshot.forEach((doc) => {
+        const user = {
+          UserID: doc.data().UserID,
+          Upvotes: doc.data().Upvotes,
+          Streak: doc.data().Streak,
+        };
+        users.push(user);
+      });
+
+      response.status(200).json(users); // Send sorted users as JSON
+    })
+    .catch((error) => {
+      console.error("Error fetching users: ", error);
+      response.status(500).send({error: error.message});
+    });
+});
+
+export const getUserData = onRequest((request, response) => {
+  const userId = request.query.userId as string; // Access the userId query parameter
+  if (!userId) {
+    response.status(400).send("No user ID provided");
+  }
+
+  const db = getFirestore();
+  db.collection("Users").doc(userId)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        response.status(404).send({message: "User not found"});
+      }
+
+      response.status(200).json(doc.data());
+    })
+    .catch((error) => {
+      response.status(500).send({error: error.message});
+    });
+});
+
+// export const getQuestCompleted = onRequest((request, response) => {
+//     const userId = request.query.userId as string;
+//     //check if most prev post was posted
+
+// });
+
+export const getPrevPostsFromUser = onRequest(async (request, response) => {
+  const userId = request.query.userId as string;
+  if (!userId) {
+    response.status(400).send("No user ID provided");
+    return;
+  }
+
+  const db = getFirestore();
+  try {
+    const userDocSnapshot = await db.collection("Users").doc(userId).get();
+    if (!userDocSnapshot.exists) {
+      response.status(404).send({message: "User not found"});
+      return;
+    }
+
+    const userPostsIds = userDocSnapshot.data()?.Posts;
+    if (!userPostsIds || !Array.isArray(userPostsIds)) {
+      response.status(404).send({message: "No posts found for this user"});
+      return;
+    }
+
+    const postsPromises = userPostsIds.map((postId: string) => {
+      return db.collection("Posts").doc(postId).get();
+    });
+
+    const postsSnapshots = await Promise.all(postsPromises);
+    const posts = postsSnapshots.map((postSnapshot) => {
+      if (!postSnapshot.exists) {
+        // Handle the case where the post doesn't exist, if necessary
+        return null;
+      }
+      return {postId: postSnapshot.id, ...postSnapshot.data()};
+    }).filter((post) => post !== null); // Remove nulls from the array of posts
+
+    response.status(200).json(posts);
+  } catch (error: any) {
+    console.error("Error fetching user's posts: ", error);
+    response.status(500).send({error: error.message});
+  }
 });
